4a5e8c9558afea6ed8953e5fdb0de7d71d36b309
Dmml0621 <35722335+Dmml0621@users.noreply.github.com>
2021-03-26 01:02:14 +0800

    Add files via upload

---
+++ js/warn.js
@@ -0,0 +1,11 @@
+function invalid_cpu(){
+    alert("Please choose your brand first!")
+}
+
+function invalid_gpu(){
+    alert("Please choose your CPU and motherboard first!")
+}
+
+function invalid_config(){
+    alert("Please choose your GPU first!")
+}
